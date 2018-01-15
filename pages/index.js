86f5280965081c40a45bd08f33b3c1d1c4fdc3a9
Ben Vincent <benjc.vincent@gmail.com>
2018-01-14 23:25:26 -0700

    Add header component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 //import './index.css';
+import Head from 'next/head';
 import App from '../components/App/App';
 
 export default () => {
   return (<div>
+    <Head>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
+    </Head>
     <App/>
     <style global jsx>{`
       body {
